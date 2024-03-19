1ed96101240068cc2082690695b5fb115d5a72b8
Nidhal-karmadi <125440528+Nidhal-karmadi@users.noreply.github.com>
2024-03-19 23:42:59 +0100

    creating the signup page
    creating the signup page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import { Routes, Route } from 'react-router-dom';
 
-import './globals.css';
+
 import SigninForm from './_auth/forms/SigninForm';
-import { Home } from './_root/pages';
 import SignupForm from './_auth/forms/SignupForm';
 import AuthLayout from './_auth/AuthLayout';
 import RootLayout from './_root/RootLayout';
+import { Home } from './_root/pages';
+import './globals.css';
+
+
 const App = () => {
     return (
         <main className="flex h-screen">
@@ -17,7 +20,7 @@ const App = () => {
                 </Route>
                 {/* private routes */}
                 <Route element={<RootLayout />}>
-                    <Route index element={<Home />} />
+                    <Route index element={<Home />}/>
                 </Route>
             </Routes>
 
